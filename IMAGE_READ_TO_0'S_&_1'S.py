09826ab566d58eb7e79c7e3a3d58ee847532ba4a
saad <l217289@lhr.nu.edu.pk>
2022-03-07 20:09:59 +0500

    changes made in line 7 and 9 used the glob library to read all the images and perform operations on them

--- IMAGE_READ_TO_0'S_&_1'S.py
+++ IMAGE_READ_TO_0'S_&_1'S.py
@@ -1,35 +1,40 @@
 import cv2
 import openpyxl
+import glob
 
 ##################################################################################
 
+path = "C:\\Users\\RAPTOR\\Desktop\\circle dataset\\*"
 
-img = cv2.imread('C:\\Users\\RAPTOR\\Desktop\\triangle dataset\\100.jpg',2)
+for file in glob.glob(path):
 
-ret, bw_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    print(file)
+    img = cv2.imread(file,2)
 
-
-pix_val = list(bw_img)
-pix_val_flat = [x for sets in pix_val for x in sets]
-#print(pix_val_flat,'\n')
-
-list_ = []
-for val in pix_val_flat:
-    if (val<=255 and val>=200):
-        list_.append(0)
-    elif(val<30):
-        list_.append(1)
-
-#print(list_)
-
-file = 'C:\\Users\\RAPTOR\\Desktop\\SHAPES-DATA-SET.xlsx'
-
-wb = openpyxl.load_workbook(filename=file)
-ws = wb['TRIANGLE DATASET']     # .get_sheet_by_name('Sheet1')
-row = ws.max_row +1
+    ret, bw_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 
 
-for col, entry in enumerate(list_, start=1):
-    ws.cell(row=row, column=col, value=entry)
+    pix_val = list(bw_img)
+    pix_val_flat = [x for sets in pix_val for x in sets]
+    #print(pix_val_flat,'\n')
 
-wb.save(file)
+    list_ = []
+    for val in pix_val_flat:
+        if (val<=255 and val>=200):
+            list_.append(0)
+        elif(val<30):
+            list_.append(1)
+
+    print(list_,"\n")
+
+    file = 'C:\\Users\\RAPTOR\\Desktop\\one.xlsx'
+
+    wb = openpyxl.load_workbook(filename=file)
+    ws = wb['Sheet1']     # .get_sheet_by_name('Sheet1')
+    row = ws.max_row +1
+
+
+    for col, entry in enumerate(list_, start=1):
+        ws.cell(row=row, column=col, value=entry)
+
+    wb.save(file)
